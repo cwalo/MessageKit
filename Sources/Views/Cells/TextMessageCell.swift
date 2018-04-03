14ded711cb4a137d73bae5a8aaf43d3fbb2bf8f0
Corey Walo <cjwalo@gmail.com>
2018-04-03 16:58:22 -0500

    Don't override textColor of messageLabel.attributedText

--- Sources/Views/Cells/TextMessageCell.swift
+++ Sources/Views/Cells/TextMessageCell.swift
@@ -80,13 +80,15 @@ open class TextMessageCell: MessageContentCell {
             switch message.data {
             case .text(let text), .emoji(let text):
                 messageLabel.text = text
+                // Needs to be set after the attributedText because it takes precedence
+                messageLabel.textColor = textColor
             case .attributedText(let text):
                 messageLabel.attributedText = text
             default:
                 break
             }
             // Needs to be set after the attributedText because it takes precedence
-            messageLabel.textColor = textColor
+//            messageLabel.textColor = textColor
         }
     }
     
